edf7d60075c49f9636c63b2f37a27b9a6ad297cf
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-01-29 15:04:47 +0300

    feat: initial mongodb config

--- models/users.py
+++ models/users.py
@@ -1,12 +1,16 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+from beanie import Document, Link
+
+from pydantic import BaseModel, EmailStr
+
 from models.events import Event
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
-    events: Optional[List[Event]]
-
+    events: Optional[List[Link[Event]]]
+    class Settings:
+        name = "users"
     class Config:
         schema_extra = {
             "example": {
@@ -15,28 +19,7 @@ class User(BaseModel):
                 "events": [],
             } 
         }
-
-class NewUser(BaseModel):
-    email: EmailStr
-    events: Optional[List[Event]]
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "fastapi@gmail.com",
-                "events": [],
-            } 
-        }
-
+        
 class UserSignIn(BaseModel):
     email: EmailStr
     password: str
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "fastapi@gmail.com",
-                "password": "strong!!!",
-                "events": [],
-            } 
-        }
